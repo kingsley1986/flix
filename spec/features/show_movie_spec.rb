69de8e2e85d4add79d00facf0c434da447f0f6ee
kingsley <chukwumakingsley1@gmail.com>
2015-12-03 12:56:48 +0200

    sidebar

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -38,4 +38,27 @@ describe "Viewing an individual movie" do
     expect(page).to have_text("Flop!")
   end
   
+
+  it "shows the movie's fans and genres in the sidebar" do
+    movie = Movie.create!(movie_attributes)
+
+
+    genre = Genre.create!(name: "Action")
+    movie.genres << genre
+
+    visit movie_url(movie)
+
+    within("aside#sidebar") do
+     
+      expect(page).to have_text(genre.name)
+    end
+  end
+
+  it "includes the movie's title in the page title" do
+    movie = Movie.create!(movie_attributes)
+
+    visit movie_url(movie)
+
+    expect(page).to have_title("Flix - #{movie.title}")
+  end
 end
